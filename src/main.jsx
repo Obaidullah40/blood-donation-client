735cf8745b048f27bbb14ec72c401d897332dc0a
Obaidullah40 <obaidullah2p17@gmail.com>
2025-07-26 02:38:24 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router'
+// import Aos from 'aos';
+// import 'aos/dist/aos.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import AuthProvider from './contexts/AuthProvider';
+import router from './router/router';
+
+
+// Aos.init();
+// const queryClient = new QueryClient();
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className="urbanist max-w-3/4 mx-auto">
+      {/* <QueryClientProvider client={queryClient}> */}
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      {/* </QueryClientProvider> */}
+    </div>
+    </StrictMode>,
+)
